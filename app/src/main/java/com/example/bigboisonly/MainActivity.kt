9a3c7a07bf8dff17d1ad74666c55faa1a98bc2e7
jagod101 <jagod101@mail.chapman.edu>
2019-09-16 20:36:30 -0700

    updates

--- app/src/main/java/com/example/bigboisonly/MainActivity.kt
+++ app/src/main/java/com/example/bigboisonly/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.bigboisonly
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -9,25 +10,31 @@ import com.example.bigboisonly.util.toggleVisibility
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var chapmanCounter: Int = 0
+    private var chapmanCounter: Long = 0
+    fun getStore() = getPreferences(Context.MODE_PRIVATE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState != null) {
-            chapmanCounter = savedInstanceState.getInt(CHAPMAN_COUNTER_KEY, 0)
+            chapmanCounter = savedInstanceState.getLong(CHAPMAN_COUNTER_KEY, 0)
+            myCounter.text = chapmanCounter.toString()
         }
         myButton.setOnClickListener {
             chapmanCounter++
-            myCounter.text = "Count: " + chapmanCounter.toString()
+            myCounter.text = "Counter: " + chapmanCounter.toString()
             myImage.rotate90()
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        getStore().edit().putLong(CHAPMAN_COUNTER_KEY, chapmanCounter).apply()
+    }
     override fun onSaveInstanceState(outState: Bundle?) {
         outState?.run {
-            putInt(CHAPMAN_COUNTER_KEY, chapmanCounter)
+            putLong(CHAPMAN_COUNTER_KEY, chapmanCounter)
         }
 
         super.onSaveInstanceState(outState)
